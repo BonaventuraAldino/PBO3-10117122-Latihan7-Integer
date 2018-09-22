a460be6a2d03c8bfc5cf664cac04f0bdf5e50d4d
BonaventuraAldino <aldysenda99@gmail.com>
2018-09-23 00:24:59 +0700

    PBO3-10117122-Latihan7-Integer

---
+++ src/pbo3/pkg10117122/latihan7/integer/PBO310117122Latihan7Integer.java
@@ -0,0 +1,36 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3.pkg10117122.latihan7.integer;
+
+/**
+ *
+ * @author lenovo
+ * 
+ * NAMA  : BONAVENTURA ALDINO SENDA SENI
+ * KELAS : PBO-3
+ * NIM   : 10117122
+ * 
+ */
+public class PBO310117122Latihan7Integer {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000; 
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int: " + i);
+        System.out.println("long : " + 1);
+    }
+}
